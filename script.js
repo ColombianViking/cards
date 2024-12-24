30114d76deee9325601d984b570df940657227c9
John Stynsberg <johnstynsberg@gmail.com>
2024-12-24 23:10:21 +0000

    version 0.1

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function addImage(root, path) {
+function addImage(root, path, clickToDismiss = false, coverCard = false, removeDuplicates = false) {
+
+
   let svg = document.createElement("div");
+  if (removeDuplicates) {
+    //create id for each card by cleaning the path
+  let id = path.replace(/[^a-zA-Z0-9]/g, "");
+  // check if card is already under this root
+  if (root.querySelector(`#${id}`)) {
+    return;
+  }
+  svg.id = id;
+  }
+  
   let glow = document.createElement("div");
   glow.classList.add("glow");
   svg.classList.add("card");
+
 
   fetch(path)
     .then((response) => response.text())
@@ -43,16 +56,65 @@ function addImage(root, path) {
   `;
   }
 
-  svg.addEventListener("mouseenter", () => {
-    bounds = svg.getBoundingClientRect();
-    document.addEventListener("mousemove", rotateToMouse);
-  });
 
-  svg.addEventListener("mouseleave", () => {
-    document.removeEventListener("mousemove", rotateToMouse);
-    svg.style.transform = "";
-    svg.style.background = "";
-  });
+  if (coverCard) {
+    svg.style.position = "relative";
+    svg.style.zIndex = 100;
+    svg.classList.add("cover");
+  } else {
+    svg.addEventListener("mouseenter", () => {
+      bounds = svg.getBoundingClientRect();
+      document.addEventListener("mousemove", rotateToMouse);
+    });
+  
+    svg.addEventListener("mouseleave", () => {
+      document.removeEventListener("mousemove", rotateToMouse);
+      svg.style.transform = "";
+      svg.style.background = "";
+    });
+  
+  }
+  
+
+  if (clickToDismiss) {
+    svg.addEventListener("click", () => {
+      if (coverCard) {
+        svg.style.animation = "packopen 1s";
+      } else if (svg.style.animation) {
+        return;
+      } 
+      else if (isCardInCollection(path)) {
+        svg.style.animation = "cardaway 1s";
+      } else {
+        svg.style.animation = "collect 1s";
+      }
+      
+      svg.addEventListener("animationend", () => {
+        svg.remove();
+      })
+      if (!coverCard) {
+        addImageToCollection(path);
+      }
+    });
+  } else {
+    // click on image to display a larger version of the image
+    // click on the larger version to dismiss it
+    svg.addEventListener("click", () => {
+      let largeImage = document.createElement("div");
+      largeImage.classList.add("largeImage");
+      fetch(path)
+        .then((response) => response.text())
+        .then((svgContent) => {
+          largeImage.innerHTML = svgContent;
+        })
+        .then((_) => {
+          largeImage.addEventListener("click", () => {
+            largeImage.remove();
+          });
+          document.body.appendChild(largeImage);
+        });
+    });
+  }
 }
 
 function getCards() {
@@ -69,27 +131,88 @@ function getCards() {
   
 }
 
+//collection
+// when an image is dismissed from a booster pack, it should be added to the collection
+// the collection should be saved to local storage
+// the collection should be loaded from local storage
+// the collection should be displayed on the page
+
+function addImageToCollection(path) {
+  //check if path is already in collection
+  if (isCardInCollection(path)) {
+    return;
+  }
+  localStorage.setItem("collection", JSON.stringify([...getCollection(), path]));
+  displayCollection();
+}
+
+function isCardInCollection(path) {
+  return getCollection().includes(path);
+}
+
+function getCollection() {
+  return JSON.parse(localStorage.getItem("collection")) || [];
+}
+
+function displayCollection() {
+  let ele = document.getElementById("cg");
+  for (let path of getCollection()) {
+    addImage(ele, path, false, false, true);
+  }
+  updateCollectionCounter();
+}
+
+function clearCollection() {
+  localStorage.removeItem("collection");
+  displayCollection();
+}
+
+function addImagesToBooster() {
+  let ele = document.getElementById("booster");
+
+  // fetch all cards and add 4 commons and 1 rare
+  // 1/8 change of getting a legendary
+
+  fetch("firstcards.json").then((response) => response.json())
+  .then((data) => {
+    let commons = data.filter(card => card.rarity === "common");
+    let rare = data.filter(card => card.rarity === "rare");
+    let legendary = data.filter(card => card.rarity === "legend");
+    let cards = [];
+    for (let i = 0; i < 4; i++) {
+      cards.push(commons[Math.floor(Math.random() * commons.length)]);
+    }
+    cards.push(rare[Math.floor(Math.random() * rare.length)]);
+    let random = Math.random();
+    if (random < 0.125) {
+      cards.push(legendary[Math.floor(Math.random() * legendary.length)]);
+      console.log("Legendary!");
+    }
+    for (let card of cards) {
+      const title = card.title || "No Title";
+      const outputFileName = `${title.replace(/ /g, "_")}.svg`;
+      addImage(ele, "output/" + outputFileName, true);
+    }
+  }).then(() => {
+      addImage(ele, "cards/cover.svg", true, true);
+    }
+  )
+}
+
+function updateCollectionCounter() {
+  let counter = document.getElementById("counter");
+  // get number of total cards and number in collection
+  let total = 0;
+  let collection = getCollection();
+  fetch("firstcards.json").then((response) => response.json())
+  .then((data) => {
+    total = data.length;
+    counter.innerText = `${collection.length}/${total}`;
+  })
+}
+
 addEventListener("load", 
     () => {
-        
-        // addImage(ele, "output/Balatro.svg")
-        // addImage(ele, "output/:eevee:.svg")
-        // addImage(ele, "output/Första_ryden.svg")
-        // addImage(ele, "output/Bibblets.svg")
-        getCards();
-        //addImage(ele, "cards/common.svg")
-        //addImage(ele, "cards/rare.svg")
-        //addImage(ele, "cards/legend.svg")
-        // addImage(ele, "modified/Beevee.svg")
-        // addImage(ele, "modified/Lincoln_ora.svg")
-        // addImage(ele, "modified/Bibblets_serien.svg")
-        // addImage(ele, "modified/Mellobetyg.svg")
-        // addImage(ele, "modified/Kebab_picen.svg")
-        // addImage(ele, "modified/Balatro.svg")
-        // addImage(ele, "modified/Dexter.svg")
-        // addImage(ele, "modified/Köttbullar.svg")
-        // addImage(ele, "modified/Monsklint.svg")
-        // addImage(ele, "modified/Papegoja.svg")
-        // addImage(ele, "modified/Ryde.svg")
+        displayCollection();
     }
 )
